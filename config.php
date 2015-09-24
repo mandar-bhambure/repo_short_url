1fffe3d0a8a5dd5ea767959adaa883c4d133b4e6
mandar-bhambure <mandarbhambure@yahoo.co.in>
2015-09-25 00:03:12 +0530

    Create config.php
    Initial Import

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+define('DB_HOST','localhost');
+define('DB_HOST_USER','root');
+define('DB_HOST_PASS','');
+define('DB_NAME','short_urls');
+define('SERVER_NAME','http://'.$_SERVER['HTTP_HOST'].'/');
+
+?>
